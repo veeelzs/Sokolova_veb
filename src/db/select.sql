476bdd8bc863701561b31f2e419b30985bfc9790
EgorRokischuk <RokischukEN@mer.ci.nsu.ru>
2025-09-15 16:08:58 +0700

    Апи студент

--- src/db/select.sql
+++ src/db/select.sql
@@ -1,2 +1,8 @@
--- Active: 1757427728965@@127.0.0.1@3306
-SELECT * FROM class;
+-- Active: 1757925478657@@127.0.0.1@3306
+CREATE TABLE student(  
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    middle_name TEXT,
+    groupId INTEGER
+);
